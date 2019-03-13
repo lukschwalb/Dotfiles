72324f02cc81afcdd6dc328fa9ebaa9444b434ee
lukschwalb <luki.schwalb@gmail.com>
2019-03-13 19:56:13 +0100

    Polybar on all monitors

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -8,4 +8,5 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-polybar top -c ~/.config/polybar/config-top.ini &
+#polybar top -c ~/.config/polybar/config-top.ini &
+for i in $(polybar -m | awk -F: '{print $1}'); do MONITOR=$i polybar top -c ~/.config/polybar/config-top.ini & done
